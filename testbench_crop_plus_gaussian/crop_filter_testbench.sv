cccffa3359272f2c954fc98ea96df05890c300d0
abdelabd <abdelabd777@gmail.com>
2025-02-04 16:44:56 -0800

    clean up submodule testbench

--- testbench_crop_plus_gaussian/crop_filter_testbench.sv
+++ testbench_crop_plus_gaussian/crop_filter_testbench.sv
@@ -3,16 +3,17 @@
 module crop_filter_testbench();
 
     //////////////////////// User parameters ////////////////////////
-    localparam FP_TOTAL = 8;
-    localparam IN_ROWS         = 9;
-    localparam IN_COLS         = 9;
-    localparam OUT_ROWS        = 3;
-    localparam OUT_COLS        = 3;
-    localparam Y_1             = 2;
-    localparam X_1             = 2;
-    localparam FP_FRAC = 0; // adjust if needed
+    localparam FP_TOTAL = 16;
+    localparam FP_FRAC = 0;
     localparam FP_INT = FP_TOTAL - FP_FRAC - 1;
-    localparam NUM_CROPS = 1; // how many “crops”/frames you want to process
+
+    localparam IN_ROWS         = 100;
+    localparam IN_COLS         = 160;
+    localparam OUT_ROWS        = 48;
+    localparam OUT_COLS        = 48;
+    localparam Y_1             = 10;
+    localparam X_1             = 10;
+    localparam NUM_CROPS       = 1; 
 
     //////////////////////// DUT signals ////////////////////////
     reg                         clk;
@@ -64,9 +65,30 @@ module crop_filter_testbench();
 		out_ready <= $urandom%2;
 	end
 
-    //////////////////////// File-handling ////////////////////////
+    //////////////////////// I/O data ////////////////////////
 
-    // Testbench I/O memory
+    string input_file_location = $sformatf("tb_data/ap_fixed_%0d_%0d/img_precrop_INDEX_%0dx%0d_to_%0dx%0dx%0d.bin",
+            FP_TOTAL, FP_INT,
+			IN_ROWS, IN_COLS,
+            OUT_ROWS, OUT_COLS,
+            NUM_CROPS);
+    string input_read_file_location = $sformatf("tb_data/ap_fixed_%0d_%0d/img_precrop_INDEX_READIN_%0dx%0d_to_%0dx%0dx%0d.bin",
+		    FP_TOTAL, FP_INT,
+			IN_ROWS, IN_COLS,
+            OUT_ROWS, OUT_COLS,
+            NUM_CROPS);
+    string output_benchmark_file_location = $sformatf("tb_data/ap_fixed_%0d_%0d/img_postcrop_INDEX_%0dx%0d_to_%0dx%0dx%0d.bin",
+            FP_TOTAL, FP_INT,
+			IN_ROWS, IN_COLS,
+            OUT_ROWS, OUT_COLS,
+            NUM_CROPS);
+    string output_file_location = $sformatf("tb_data/ap_fixed_%0d_%0d/cropfilter_pred_INDEX_%0dx%0d_to_%0dx%0dx%0d.bin",
+            FP_TOTAL, FP_INT,
+			IN_ROWS, IN_COLS,
+            OUT_ROWS, OUT_COLS,
+            NUM_CROPS);
+    
+    // I/O memory
     logic [FP_TOTAL-1:0] input_mem  [IN_ROWS*IN_COLS-1:0];
     logic [FP_TOTAL-1:0] output_mem [OUT_ROWS*OUT_COLS-1:0];
     logic [FP_TOTAL-1:0] output_benchmark_mem [OUT_ROWS*OUT_COLS-1:0];
@@ -121,48 +143,29 @@ module crop_filter_testbench();
 
     initial begin
 
-        //////////////////////// 1. Toggle reset ////////////////////////
-        reset = 1'b1;   
-        #(CLOCK_PERIOD*2);
+        $display("\ninput_file_location = %0d", input_file_location);
+		$display("output_benchmark_file_location = %0d", output_benchmark_file_location);
+		$display("input_read_file_location = %0d", input_read_file_location);
+		$display("output_file_location = %0d\n", output_file_location);
 
-        reset = 1'b0;
-        #10;
-
-        //////////////////////// 2. Load input and benchmark data ////////////////////////
-
+        //////////////////////// 1. Load input and benchmark data ////////////////////////
+	    
         // input data
-        $readmemb($sformatf("tb_data/ap_fixed_%0d_%0d/tb_input_INDEX_%0dx%0d_to_%0dx%0dx%0d.bin",
-            FP_TOTAL,
-            FP_INT,
-            IN_ROWS, IN_COLS,
-            OUT_ROWS, OUT_COLS,
-            NUM_CROPS), input_mem);
+        $readmemb(input_file_location, input_mem);
 
-        $readmemb($sformatf("tb_data/ap_fixed_%0d_%0d/tb_benchmark_output_INDEX_%0dx%0d_to_%0dx%0dx%0d.bin",
-            FP_TOTAL,
-            FP_INT,
-            IN_ROWS, IN_COLS,
-            OUT_ROWS, OUT_COLS,
-            NUM_CROPS), output_benchmark_mem);
+        // output benchmark data
+        $readmemb(output_benchmark_file_location, output_benchmark_mem);
 
-        //////////////////////// 3. Wait for computation to complete ////////////////////////
-    //    #(10*IN_ROWS*IN_COLS*CLOCK_PERIOD+1000);
+        //////////////////////// 2. Wait for computation to complete ////////////////////////
+
+        // Toggle reset
+        reset = 1'b1;  #(CLOCK_PERIOD*2); reset = 1'b0; #10;
+        
         wait(finished);
 
-        $display("input_file location = %0d", $sformatf("tb_data/ap_fixed_%0d_%0d/tb_input_INDEX_%0dx%0d_to_%0dx%0dx%0d.bin",
-            FP_TOTAL,
-            FP_INT,
-            IN_ROWS, IN_COLS,
-            OUT_ROWS, OUT_COLS,
-            NUM_CROPS));
         //////////////////////// 3. Save output, close files ////////////////////////
         // Input-read
-        input_read_file = $fopen($sformatf("tb_data/ap_fixed_%0d_%0d/tb_input_READ_INDEX_%0dx%0d_to_%0dx%0dx%0d.bin",
-            FP_TOTAL,
-            FP_INT,
-            IN_ROWS, IN_COLS,
-            OUT_ROWS, OUT_COLS,
-            NUM_CROPS), "wb");
+        input_read_file = $fopen(input_read_file_location, "wb");
         if (input_read_file == 0) begin
             $display("Error: Could not open input-read file for writing.");
             $stop;
@@ -170,14 +173,12 @@ module crop_filter_testbench();
         else begin
             $display("Could indeed open input-read file for writing.");
         end
+        for (i=0; i<IN_ROWS*IN_COLS; i=i+1) begin
+            $fwrite(input_read_file, "%b\n", input_mem[i]);
+        end
 
         // Output
-        output_file = $fopen($sformatf("tb_data/ap_fixed_%0d_%0d/tb_output_INDEX_%0dx%0d_to_%0dx%0dx%0d.bin",
-            FP_TOTAL,
-            FP_INT,
-            IN_ROWS, IN_COLS,
-            OUT_ROWS, OUT_COLS,
-            NUM_CROPS), "wb");
+        output_file = $fopen(output_file_location, "wb");
         if (output_file == 0) begin
             $display("Error: Could not open output file for writing.");
             $stop;
@@ -185,16 +186,10 @@ module crop_filter_testbench();
         else begin
             $display("Could indeed open output file for writing.");
         end
-
-
         for (i=0; i<OUT_ROWS*OUT_COLS; i=i+1) begin
             $fwrite(output_file, "%b\n", output_mem[i]);
         end
-        for (i=0; i<IN_ROWS*IN_COLS; i=i+1) begin
-            $fwrite(input_read_file, "%b\n", input_mem[i]);
-        end
-
-        $fclose(input_file);
+        
         $fclose(input_read_file);
         $fclose(output_file);
         
